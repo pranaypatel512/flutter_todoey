8cf1f9d6d147640011cef2df11fb7524d872aa8b
pRaNaY <www.pranay1337.com@gmail.com>
2023-08-08 17:17:03 +0530

    Design primary layout for tasks screen

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -9,16 +9,62 @@ class _TasksScreenState extends State<TasksScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.lightBlueAccent,
-        appBar: AppBar(
-          // TRY THIS: Try changing the color here to a specific color (to
-          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-          // change color while the other colors stay the same.
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text('Todoey'),
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: const Text('Todoey'),
+      ),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Container(
+          padding:
+              const EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 30.0,
+                child: Icon(
+                  Icons.list,
+                  size: 30.0,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'Todoey',
+                style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.w700),
+              ),
+              Text(
+                '12 Tasks',
+                style: TextStyle(fontSize: 14.0),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+            ],
+          ),
         ),
-        body: Center());
+        Expanded(
+            child: Container(
+          decoration: BoxDecoration(
+              color: Colors.deepPurple.withOpacity(0.1),
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10.0))),
+        ))
+      ]),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+    );
   }
 }
