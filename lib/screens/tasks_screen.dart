ff2b24fcd67b7f6037dd22e8def9425a666ad30a
pRaNaY <www.pranay1337.com@gmail.com>
2023-08-08 16:42:47 +0530

    refactor todo screen

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatefulWidget {
+  @override
+  State<TasksScreen> createState() => _TasksScreenState();
+}
+
+class _TasksScreenState extends State<TasksScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.lightBlueAccent,
+        appBar: AppBar(
+          // TRY THIS: Try changing the color here to a specific color (to
+          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+          // change color while the other colors stay the same.
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text('Todoey'),
+        ),
+        body: Center());
+  }
+}
