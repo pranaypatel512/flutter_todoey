aa92e6fb353d8a8e9a6711779653b6d9dd4e8c3e
pRaNaY <www.pranay1337.com@gmail.com>
2023-08-09 13:03:30 +0530

    refactor state once user check item

--- lib/widgets/task_list_item.dart
+++ lib/widgets/task_list_item.dart
@@ -1,18 +1,50 @@
-
-
 import 'package:flutter/material.dart';
 
-class TasksListItem extends StatelessWidget {
-  TasksListItem({required this.text});
+class TasksListItem extends StatefulWidget {
+  TasksListItem({super.key, required this.text});
   String text;
+
+  @override
+  State<TasksListItem> createState() => _TasksListItemState();
+}
+
+class _TasksListItemState extends State<TasksListItem> {
+  bool? isChecked = false;
+  void checkBoxCallBack(bool? checkBoxState) {
+    setState(() {
+      isChecked = checkBoxState;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(text),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
+      title: Text(
+        widget.text,
+        style: TextStyle(
+            decoration: isChecked ?? false
+                ? TextDecoration.lineThrough
+                : TextDecoration.none),
       ),
+      trailing: TaskCheckBox(
+        checkState: isChecked,
+        toggleCheckboxState: checkBoxCallBack,
+      ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  const TaskCheckBox(
+      {super.key, required this.checkState, this.toggleCheckboxState});
+
+  final bool? checkState;
+  final Function(bool?)? toggleCheckboxState;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
